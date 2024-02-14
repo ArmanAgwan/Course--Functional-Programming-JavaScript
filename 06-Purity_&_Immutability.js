4f08a3d3b8ba9862cf883c2cdcbf8d4ea8da7942
ArmanAgwan <armanagwa@gmail.com>
2024-02-14 11:58:42 +0530

    code

---
+++ 06-Purity_&_Immutability.js
@@ -0,0 +1,17 @@
+// Pure Function //
+
+// let num = 10
+// const add3 = x => {
+//  num++
+//  return x+3
+// }
+// add3(7)
+
+
+// Immutabilty //
+
+
+// const array = [1,2,3]
+// const multiplyBy2 = num => num*2
+// const result = array.map(multiplyBy2) // [2,4,6]
+// const newResult = array.map(multiplyBy2) // [2,4,6]
