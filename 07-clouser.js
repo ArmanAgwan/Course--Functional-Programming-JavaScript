757a4d91ae66ddbd0849fd45881fc7d7071da42d
ArmanAgwan <armanagwa@gmail.com>
2024-02-14 12:23:29 +0530

    code

---
+++ 07-clouser.js
@@ -0,0 +1,48 @@
+                    // Clouser //
+
+// const multiplyBy2 = inputNumber => inputNumber*2;
+// const output = multiplyBy2(7);
+// const newOutput = multiplyBy2(3)
+
+
+                   // Clouser Under The Hood //
+
+
+// const functionCreator = () => {
+//     let counter = 0
+//     const add3 = (num) => {
+//     const result = num+3
+//     return result
+//     }
+//     return add3
+//    }
+//    const generatedFunc = functionCreator()
+//    const result = generatedFunc(2) //5
+
+
+                    // Inner Function //
+
+
+// const outer = () => {
+//     let counter = 0;
+//     const incrementCounter = () => {
+//     counter ++;
+//     }
+//     incrementCounter();
+//    }
+//    outer();
+
+
+                      // Outer Function //
+
+
+// const outer = () => {
+//     let counter = 0;
+//     const incrementCounter = () => {
+//     counter ++;
+//     }
+//     return incrementCounter
+//    }
+//    const newFunction = outer();
+//    newFunction()
+//    newFunction()
